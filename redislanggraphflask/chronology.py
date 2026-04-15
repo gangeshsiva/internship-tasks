4c5056389f465fa52498cdcd627124d61cdc70db
11201 - Gangeshvar S <gangeshvar.sivasubramani@adamsbridge.tech>
2026-04-15 17:41:56 +0530

    Add files via upload

--- redislanggraphflask/chronology.py
+++ redislanggraphflask/chronology.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from icdcpt import ICD_CPT_SCT_code
 
 app = Flask(__name__)
 
@@ -6,10 +7,12 @@ app = Flask(__name__)
 def chronology():
     data = request.json
     text = data.get("text", "")
+    coordinates = text["coordinates"]
+    texts = text["texts"]
 
-    return jsonify({
-        "message": "this message is from chronology page"
-    })
+    output=ICD_CPT_SCT_code(texts, coordinates)
+
+    return output 
 
 if __name__ == "__main__":
     app.run(port=9012, debug=True)
